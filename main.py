3512b4aad370b63c69aa9248170bc1ef1b1a770e
Andrew Roddy <andrewroddy7@gmail.com>
2026-02-28 23:41:06 -0500

    Get all repos

--- main.py
+++ main.py
@@ -22,12 +22,42 @@ def getEnv():
 
     return env
 
+def getRepos(env):
+    repo_urls = set()
+
+    # Getting stuff from GitHub
+    GITHUB_PAT = env.get("GITHUB_PAT")
+    headers = {'Authorization': f'Bearer {GITHUB_PAT}'}
+
+    urls = [
+        # All Public Repositories
+        'https://api.github.com/users/AndrewRoddy/repos?type=all&per_page=100',
+
+        # All private repositories
+        'https://api.github.com/user/repos?type=all&per_page=100'
+    ]
+    
+    # Does the request on both URLs
+    for url in urls:
+        r = requests.get(url=url, headers=headers)
+        for repo in r.json():
+            repo_urls.add(repo["url"])
+
+    # Converts URL's to a list then sorts them
+    repo_urls = list(repo_urls)
+    repo_urls = sorted(repo_urls)
+
+    # Prints all repo names and counts
+    # for temp_url in repo_urls: print(temp_url)
+    # print(len(repo_urls))
+
+    return repo_urls
+
 def main():
     env = getEnv() # Holds all environment variables 
 
     OBSIDIAN_PATH = env.get("OBSIDIAN_PATH")
     DAILY_NOTES_FOLDER = env.get("DAILY_NOTES_FOLDER")
-    GITHUB_PAT = env.get("GITHUB_PAT")
 
     # DAILY_NOTES_PATH = OBSIDIAN_PATH + "/" + DAILY_NOTES_FOLDER
 
@@ -35,35 +65,10 @@ def main():
     # print(os.chdir(DAILY_NOTES_PATH))
     # print(os.listdir('.'))
 
-    urls = set()
-
-    # Getting stuff from GitHub
-    headers = {'Authorization': f'Bearer {GITHUB_PAT}'}
-
-    url = 'https://api.github.com/users/AndrewRoddy/repos?type=all&per_page=100'
-
-    r = requests.get(url=url, headers=headers)
-
-
-    for repo in r.json():
-        urls.add(repo["url"])
+    repo_urls = getRepos(env)
     
-    url = 'https://api.github.com/user/repos?type=all&per_page=100'
 
-    r = requests.get(url=url, headers=headers)
 
-    for repo in r.json():
-        urls.add(repo["url"])
-
-    urls = list(urls)
-
-    urls = sorted(urls)
-
-    for url in urls:
-        print(url)
-
-    print(len(urls))
-    
 
 if __name__ == "__main__":
     main() 
