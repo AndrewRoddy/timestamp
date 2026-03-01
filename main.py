a48ce182aa2f8d033310bca5ba677e10b9518f5d
Andrew Roddy <andrewroddy7@gmail.com>
2026-02-28 23:07:02 -0500

    Gets all public and private repos

--- main.py
+++ main.py
@@ -36,28 +36,25 @@ def main():
     # print(os.listdir('.'))
 
     # Getting stuff from GitHub
-    url = 'https://api.github.com/graphql'
-    json = { 'query' : """
-    { viewer 
-        { repositories(first: 30) 
-            { totalCount pageInfo 
-                { 
-                    hasNextPage endCursor 
-                } 
-                edges {
-                    node {
-                        name 
-                    } 
-                } 
-            } 
-        } 
-    }
-    """ }
     api_token = GITHUB_PAT
     headers = {'Authorization': 'token %s' % api_token}
 
-    r = requests.post(url=url, json=json, headers=headers)
-    print (r.text)
+    url = 'https://api.github.com/users/AndrewRoddy/repos?type=all?per_page=100'
+
+    r = requests.get(url=url, headers=headers)
+
+
+    for repo in r.json():
+        print(repo["url"])
+    print()
+    
+    url = 'https://api.github.com/user/repos?type=all?per_page=100'
+
+    r = requests.get(url=url, headers=headers)
+
+    for repo in r.json():
+        print(repo["url"])
+    
 
 if __name__ == "__main__":
     main() 
