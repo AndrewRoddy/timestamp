d0eecdf1ec9a942022e201561619b5385ce0bb4f
Andrew Roddy <andrewroddy7@gmail.com>
2026-02-28 21:34:48 -0500

    Made it read from the envirnment variables properly

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ import os
 
 def getEnv():
     env = {}
+
+    # Opens the file
     with open(".env", "r") as file:
         content = file.read()
-        content = content.split("=")
-        # content[0] is OBSIDIAN_PATH
-        # content[0].strip("\"") is "C:/Folder"
-        env[content[0]]=content[1].strip("\"")
+        
+        lines = content.split("\n") # Splits by line
+
+        for line in lines:
+
+            line = line.split("=") # Splits into key and value
+            key = line[0]
+            value = line[1]
+            
+            value = value[1:-1] # Removes quotes
+            env[key] = value # Sets it in the dictionary
 
     return env
 
 def main():
     env = getEnv() # Holds all environment variables 
 
-    print(env.get("OBSIDIAN_PATH"))
-
-    # print(os.chdir())
+    OBSIDIAN_PATH = env.get("OBSIDIAN_PATH")
+    print(OBSIDIAN_PATH)
+    # print(os.chdir(OBSIDIAN_PATH))
     # print(os.listdir('.'))
 
 
