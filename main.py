f98dba683b755b0f99d6fdd4d066cd2314f2f104
Andrew Roddy <andrewroddy7@gmail.com>
2026-02-28 21:18:53 -0500

    Created function that gets all of the environment variables

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import os
 
+def getEnv():
+    env = {}
+    with open(".env", "r") as file:
+        content = file.read()
+        content = content.split("=")
+        # content[0] is OBSIDIAN_PATH
+        # content[0].strip("\"") is "C:/Folder"
+        env[content[0]]=content[1].strip("\"")
+
+    return env
+
 def main():
-    
-    print("hello")
-    print(os.getcwd())
+    env = getEnv() # Holds all environment variables 
+
+    print(env.get("OBSIDIAN_PATH"))
+
+    # print(os.chdir())
+    # print(os.listdir('.'))
+
 
 if __name__ == "__main__":
     main() 
