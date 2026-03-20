76b20619f0367f38327f31865a018b02832bf984
Andrew Roddy <andrewroddy7@gmail.com>
2026-03-20 17:44:49 -0400

    Updated the tests

---
+++ general/test.py
@@ -0,0 +1,30 @@
+from . import getEnv
+
+def test_getEnv():
+    ENV = getEnv("general/envTest.env")
+    # Required ################
+    assert ENV["OBSIDIAN_PATH"]=="C:/VaultName"
+    assert ENV["DAILY_NOTES_FOLDER"]=="FolderName"
+    # Copy from Daily Notes as "Custom format"
+    assert ENV["CUSTOM_FORMAT"]=="YYYY/MM-MMMM/YYYY-MM-DD-dddd"
+    # Named "Template file Location"
+    assert ENV["DAILY_NOTE_TEMPLATE"]=="FolderName/Template"
+    # Needs to follow the IANA time zone identifier
+    assert ENV["TIME_ZONE"]=="America/New_York"
+    assert ENV["BIRTHDAY"]=="YYYY-MM-DD"
+    ###########################
+
+    ##### 👾 GitHub Commits
+    assert ENV["GITHUB_USERNAME"]=="MyUsername"
+    assert ENV["GITHUB_EMAIL"]=="email@email.com"
+    # Make sure it is under 365 days expiring
+    #     if you are a part of any organizations
+    assert ENV["GITHUB_PAT"]=="ubrpiguberipgbweriughuqyfpowqiurwqiopuofhaskjfbvfm"
+    assert "GITHUB_START_DATE"  not in ENV
+    assert "GITHUB_LAST_UPDATE" not in ENV
+    ###########################
+
+    ##### 🎮 Steam Achievements
+    assert ENV["STEAM_API_KEY"]=="hjaklhfjasdhlfkjadhjkflfa"
+    assert ENV["STEAM_ID"]=="123640923245236"
+    ###########################
--- test.py
+++ test.py
@@ -2,3 +2,4 @@ from github.test import test_utcToZone
 # from github.test import test_isContributor
 from github.test import test_formatCommits
 from filepaths.test import test_getPath
+from general.test import test_getEnv
