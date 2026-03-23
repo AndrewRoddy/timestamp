ac5c20a2dfc74a88ea0da3ce5c2fbc6f8f493884
Andrew Roddy <andrewroddy7@gmail.com>
2026-03-23 15:16:03 -0400

    Finished unit test setup

--- github/tests/test_formatCommits.py
+++ github/tests/test_formatCommits.py
@@ -1,6 +1,6 @@
 from github import formatCommits
 
-def test_formatCommits():
+def test_formatCommits_default():
     commits = [
         ["2026-02-28", "19:10:50", "Start of project", "timestamp"],
         ["2026-02-28", "19:20:45", "Started using uv", "other repo"],
@@ -23,14 +23,8 @@ def test_formatCommits():
     ]
     formatted = formatCommits(commits)
 
-
     assert formatted["2026-02-28"] == """19:10:50 (timestamp) Start of project
 19:20:45 (other repo) Started using uv"""
-
-
-
-    assert formatted["2026-03-19"] == """16:18:19 (timestamp) This commit is a test to see what I can do here !@#$%^&*()_+<>?:\"{}|,./;'[]\\`~1234567890\\\" \"\" \"\\\"\"
-16:18:43 (timestamp) Merge branch 'format-commits'"""
 
     assert formatted["2026-02-28"]== "19:10:50 (timestamp) Start of project\n19:20:45 (other repo) Started using uv"
 
@@ -41,3 +35,13 @@ def test_formatCommits():
     assert formatted["2026-03-05"] == "16:38:20 (timestamp) Fixed pagination for commits\n16:53:00 (timestamp) Pulled commit date properly\n16:58:28 (timestamp) Added time zone but I don't think it does anything\n17:48:24 (timestamp) Fixed empty response\n18:14:15 (my repo) Thinking about how to manually convert to time zone\n22:02:36 (timestamp) Seperated into github folder and added pytest\n22:17:57 (timestamp) Moved environment variable getting to a general module\n22:59:44 (timestamp) Moved github functions to proper place"
 
     assert formatted["2026-03-06"] == "09:31:33 (the repo) Implemented proper time zone conversion\n09:31:51 (timestamp) Merge branch 'utc-to-tz'"
+
+def test_formatCommits_special_characters():
+    commits = [
+        ["2026-03-19", "16:18:19", "This commit is a test to see what I can do here !@#$%^&*()_+<>?:\"{}|,./;'[]\\`~1234567890\\\" \"\" \"\\\"\"", "timestamp"],
+        ["2026-03-19", "16:18:43", "Merge branch 'format-commits'", "timestamp"],
+    ]
+    formatted = formatCommits(commits)
+
+    assert formatted["2026-03-19"] == """16:18:19 (timestamp) This commit is a test to see what I can do here !@#$%^&*()_+<>?:\"{}|,./;'[]\\`~1234567890\\\" \"\" \"\\\"\"
+16:18:43 (timestamp) Merge branch 'format-commits'"""
